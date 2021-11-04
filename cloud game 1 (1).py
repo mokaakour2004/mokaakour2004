4cbd50e03c57f8b903097a1e4cd40e246520d9f0
mokaakour2004 <93605613+mokaakour2004@users.noreply.github.com>
2021-11-04 18:10:32 +0200

    Add files via upload

---
+++ cloud game 1 (1).py
@@ -0,0 +1,107 @@
+print("Cloud game 1")
+print("let's play a game")
+print("you choose a number and the program will tell you the color hiding on your number.")
+print("you should chose from 1 to 10.")
+
+a = int(input("Make your chose here "))
+
+if a == 1:
+    print("GREEN")
+elif a == 2:
+    print("RED")
+elif a == 3:
+    print("BLUE")
+elif a == 4:
+    print("GOLDEN")
+elif a == 5:
+    print("BLACK")
+elif a == 6:
+    print("PINK")
+elif a == 7:
+    print("ORANGE")
+elif a == 8:
+    print("GRAY")
+elif a == 9:
+    print("PURPLE")
+elif a == 10:
+    print("WHITE")
+a = int(input("second step press 0"))
+if a == 15:
+    print("GREEN")
+elif a == 16:
+    print("RED")
+elif a == 17:
+    print("BLUE")
+elif a == 18:
+    print("GOLDEN")
+elif a == 19:
+    print("BLACK")
+elif a == 20:
+    print("PINK")
+elif a == 21:
+    print("ORANGE")
+elif a == 22:
+    print("GRAY")
+elif a == 23:
+    print("PURPLE")
+elif a == 24:
+    print("WHITE")
+print("you need to choose from 15 to 24 if you get the same color that you got before you will win the game ")
+a = int(input("Make your chose here "))
+if a == 1:
+    print("GREEN")
+elif a == 15:
+    print("GREEN")
+if a == 2:
+    print("RED")
+elif a == 16:
+    print("RED")
+if a == 3:
+    print("BLUE")
+elif a == 17:
+    print("BLUE")
+if a == 4:
+    print("GOLDEN")
+elif a == 18:
+    print("GOLDEN")
+if a == 5:
+    print("BLACK")
+elif a == 19:
+    print("BLACK")
+if a == 6:
+    print("ORANGE")
+elif a == 20:
+    print("ORANGE")
+if a == 7:
+    print("GRAY")
+elif a == 21:
+    print("GRAY")
+if a == 8:
+    print("PURPLE")
+elif a == 22:
+    print("PURPLE")
+if a == 9:
+    print("WHITE")
+elif a == 23:
+    print("WHITE")
+if a == 10:
+    print("pink")
+elif a == 24:
+    print("pink")
+a = int(input("press 1 if you got the same color or 0 if you did not"))
+if a == 1:
+    print("win")
+elif a == 0:
+    print("lose")
+print("Play again")
+
+
+
+
+
+
+
+
+
+
+
